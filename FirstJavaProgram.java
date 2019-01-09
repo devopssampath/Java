99b8f08a341b83d5d48296d8abe3228d94b5a8f7
devopssampath <45712866+devopssampath@users.noreply.github.com>
2019-01-09 20:33:12 +0530

    Update FirstJavaProgram.java

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,6 +1,6 @@
 public class FirstJavaProgram {
   public static void main(String[] args){
     System.out.println("This is my first program in java");
-	System.out.println("New line added to the Program");
+	System.out.println("New line added to the Program CHANGED");
   }//End of main
 }//End of FirstJavaProgram Class
