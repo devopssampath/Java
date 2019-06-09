d777dc2aa466f6d1d2695e169b385cdaf8f4b3a9
devopssampath <45712866+devopssampath@users.noreply.github.com>
2019-06-09 16:21:28 +0530

    Update FirstJavaProgram.java

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,7 +1,14 @@
+import java.util.Date;
 public class FirstJavaProgram {
   public static void main(String[] args){
+     // Instantiate a Date object
+      Date date = new Date();
+
     System.out.println("This is my first program in java");
-	System.out.println("New line added to the Program. Changed");
-	System.out.println("Pipeline 2 example");  
+    System.out.println("New line added to the Program. Changed");
+    System.out.println("Pipeline 2 example");  
+    // display time and date using toString()
+    System.out.println(date.toString());
   }//End of main
 }//End of FirstJavaProgram Class
+
